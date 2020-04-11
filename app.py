9dcc3795a3ca54af3464c8c64f6e9f1ad79ab87d
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-11 12:02:07 +1000

    playing with variables

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name)
+print("but he didnt like being " + character_age + ".")
