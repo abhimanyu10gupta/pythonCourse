b305d6cf9226cb2282ecf1a502f12df193ae7204
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-11 15:38:32 +1000

    list functions

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+lucky_numbers = [4, 8, 15, 16, 23, 42]
 
+friends.append("Mikey")
 print(friends)
 
-print(friends[2])
-print(friends[-1])
-print(friends[-2])
-print(friends[1:4])
+lucky_numbers.reverse()
+print(lucky_numbers)
 
-friends[1] = "Noah"
+friends2= friends.copy()
+print(friends2)
 
+friends.sort()
 print(friends)
+
+friends.extend(lucky_numbers)
+friends.insert(2, "Neo")
+print(friends)
+
+friends.remove("Jim")
+print(friends)
+
+friends.pop()
+print(friends)
+
+print(friends.index("Kevin"))
+
+print(friends.count("Neo"))
+
+friends.clear()
+print(friends)
+
+
