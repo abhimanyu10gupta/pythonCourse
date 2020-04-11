f5a9cd1899dca3d5e72933f94ff6fa7c5f1fab1f
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-11 12:05:59 +1000

    playing with integers and booleans

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 character_name = "Tom"
-character_age = "50"
+character_age = 50
+is_male = True
 
 print("There once was a man named " + character_name + ",")
 print("he was " + character_age + " years old.")
 
-character_name = "Mike"
+character_name = " Mike"
 print("He really liked the name " + character_name)
 print("but he didnt like being " + character_age + ".")
