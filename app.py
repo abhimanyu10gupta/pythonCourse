0496eec2ae4e60b841b4ead25b7f74c40b38c434
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-11 12:23:02 +1000

    playing with strings

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
+print("Giraffe\Academy")
+
+phrase = "Giraffe Academy"
+
+print("\n" + phrase)
+
+print(phrase + " is cool")
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+
+print(len(phrase))
+print(phrase[0])
+
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+
+print(phrase.replace("Giraffe", "Elephant"))
+
+
+
