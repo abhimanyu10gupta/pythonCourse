191d9556fde074d3c12a8a09bde6f1c25bb41ae8
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-11 12:03:24 +1000

    change name

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-character_name = "John"
-character_age = "35"
+character_name = "Tom"
+character_age = "50"
 
 print("There once was a man named " + character_name + ",")
 print("he was " + character_age + " years old.")
+
+character_name = "Mike"
 print("He really liked the name " + character_name)
 print("but he didnt like being " + character_age + ".")
