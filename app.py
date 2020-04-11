bb3d78bfb22ee86c84c8daf29ce6b896088625ed
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 00:39:56 +1000

    playing with if statements with and or not

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter a number: ")
+is_male = True
+is_tall = True
+if is_male or is_tall:
+    print("you are a male or tall or both")
+else:
+    print("you are neither male or tall")
 
-result = float(num1) + float(num2)
-print(result)
+if is_male and is_tall:
+    print("you are male and tall")
+elif is_male and not(is_tall):
+    print("you are male but not tall")
+elif not(is_male) and not is_tall:
+    print("you are female but not tall")
+else:
+    print("you are either not male or not tall ot both")
