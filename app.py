378d00819d414c895a4984cebdd7ba9028b9517f
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 00:49:23 +1000

    add all operators

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter a number: ")
+num1 = float(input("Enter a number: "))
+op = input("Enter an operator: ")
+num2 = float(input("Enter a number: "))
 
-result = float(num1) + float(num2)
-print(result)
+if op == "+":
+    print(num1+num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "-":
+    print(num1 - num2)
+else:
+    print("Invalid Operator")
+
