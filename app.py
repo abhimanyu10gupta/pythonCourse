679a21e4a8577fb55ad453a1e19d5b7276a18f9c
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-11 12:46:02 +1000

    playing with numbers

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+from math import *
+
+print(50)
+print(50.34098)
+print(-50)
+print(3+498)
+print(50+56.93)
+print(50*56.93)
+print(4*5/(3*(5+4)))
+print(10%3)
+
+my_num = -5
+print(my_num)
+print(my_num *5)
+
+print(str(my_num) + " my fav number")
+print(abs(my_num))
+print(pow(abs(my_num), 3))
+
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.4))
+print(round(3.5))
+
+#from math module
+print(floor(3.7))
+print(ceil(4.1))
+print(sqrt(pi))
+
