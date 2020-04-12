42f6d58189bffc6309e91c444458121a7b3e32ac
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 11:32:24 +1000

    initial

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-num1 = float(input("Enter a number: "))
-op = input("Enter an operator: ")
-num2 = float(input("Enter a number: "))
-
-if op == "+":
-    print(num1+num2)
-elif op == "/":
-    print(num1/num2)
-elif op == "*":
-    print(num1 * num2)
-elif op == "-":
-    print(num1 - num2)
-else:
-    print("Invalid Operator")
-
+try:
+    number = 10/0
+    number1 = int(input("enter your number: "))
+    print(number)
+except ZeroDivisionError as err:
+    print(err)
+except ValueError:
+    print("invalid input")
