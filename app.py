3cb33d3033d1b71deebefc67c924b035e6df18c8
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 11:03:14 +1000

    exponential function

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-num1 = float(input("Enter a number: "))
-op = input("Enter an operator: ")
-num2 = float(input("Enter a number: "))
+print(2**3)
 
-if op == "+":
-    print(num1+num2)
-elif op == "/":
-    print(num1/num2)
-elif op == "*":
-    print(num1 * num2)
-elif op == "-":
-    print(num1 - num2)
-else:
-    print("Invalid Operator")
-
+def raise_to_power(base, pow):
+    result = 1
+    for index in range(pow):
+       result = result * base
+    return result
+print(raise_to_power(2,3))
