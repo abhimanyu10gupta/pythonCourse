83498aea0cf83e54c6f59de7c940540df8ef4580
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 11:15:29 +1000

    build a translator

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-num1 = float(input("Enter a number: "))
-op = input("Enter an operator: ")
-num2 = float(input("Enter a number: "))
+# every vowel is gonna become a 'g'
 
-if op == "+":
-    print(num1+num2)
-elif op == "/":
-    print(num1/num2)
-elif op == "*":
-    print(num1 * num2)
-elif op == "-":
-    print(num1 - num2)
-else:
-    print("Invalid Operator")
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
 
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("input the phrase: ")))
