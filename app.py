1579e014e8a4e0222a32b6014b111de3f61cee5d
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 12:04:54 +1000

    play with write files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-employee_file = open("employees.txt", 'r')
+employee_file = open("employees.txt", 'r+')
 
 print(employee_file.readable())
 print(employee_file.readlines())
@@ -9,5 +9,7 @@ print(employee_file.read())
 
 for employee in employee_file.readlines():
     print(employee)
+
+employee_file.write("\ntoby = human resources")
 
 employee_file.close()
