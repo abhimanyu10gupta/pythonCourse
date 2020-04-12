dd7e3f08bbbd698f6e8af77c662124900397cc49
Aabhimanyu Gupta <abhimanyu10gupta@github.com>
2020-04-12 10:54:53 +1000

    build a guessing game

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+secret_word = "girrafe"
+guess =""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+while(guess != secret_word and not(out_of_guesses)):
+    if guess_count < guess_limit:
+        guess = input("Enter Guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
-
-print("done with loop")
+if out_of_guesses:
+    print("out of guesses")
+else:
+    print("You Win")
